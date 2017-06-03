87789e197f358aae8f5f024bcaab6d9eb782a1a4
morinpic <morinpic0518@gmail.com>
2017-06-03 15:54:19 +0900

    first commit

---
+++ iOS_LINE_UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS_LINE_UI
+//
+//  Created by 森健太 on 2017/06/03.
+//  Copyright © 2017年 Kenta Mori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
